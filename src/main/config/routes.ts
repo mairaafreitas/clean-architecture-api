4d51ee924e1c53de1178ca16208da9a9ada59106
mairaafreitas <maira.oliveirafreitas@gmail.com>
2023-09-25 09:59:54 -0300

    feat: set register router config

---
+++ src/main/config/routes.ts
@@ -0,0 +1,11 @@
+/* eslint-disable n/no-path-concat */
+import { Router, type Express } from 'express'
+import { readdirSync } from 'fs'
+
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+  readdirSync(`${__dirname}/routes`).map(async file => {
+    (await import (`${__dirname}/routes/${file}`)).default(router)
+  })
+}
