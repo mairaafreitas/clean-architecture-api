dd31bcbc99b92f74d903acd5a27e539ce5246ec0
mairaafreitas <maira.oliveirafreitas@gmail.com>
2023-09-25 08:04:29 -0300

    feat: add http helper

--- src/web-controllers/util/http-helper.ts
+++ src/web-controllers/util/http-helper.ts
@@ -4,3 +4,13 @@ export const created = (data: any): HttpResponse => ({
   statusCode: 201,
   body: data
 })
+
+export const badRequest = (data: any): HttpResponse => ({
+  statusCode: 400,
+  body: data
+})
+
+export const serverError = (data: any): HttpResponse => ({
+  statusCode: 500,
+  body: data
+})
