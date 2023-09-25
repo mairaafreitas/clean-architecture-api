5c619bae4191861e2901ffd6ce39eb6493a82a18
mairaafreitas <maira.oliveirafreitas@gmail.com>
2023-09-25 08:40:51 -0300

    feat: apply cors middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,4 +1,8 @@
 import { type Express } from 'express'
 import { bodyParser } from '@/main/config/middleware/body-parser'
+import { cors } from '@/main/config/middleware/cors'
 
-export default (app: Express): void => { app.use(bodyParser) }
+export default (app: Express): void => {
+  app.use(bodyParser)
+  app.use(cors)
+}
---
+++ test/main/middleware/cors.spec.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('CORS Middleware', () => {
+  test('should enable cors', async () => {
+    app.post('/test_cors', (req, res) => {
+      res.send()
+    })
+    await request(app).get('/test_cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-headers', '*')
+      .expect('access-control-allow-methods', '*')
+  })
+})
