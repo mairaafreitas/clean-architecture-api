7d652e9499cf1642594fc23eee68256fbb1c26d9
mairaafreitas <maira.oliveirafreitas@gmail.com>
2023-10-02 09:22:32 -0300

    test: add test to mongodb user repository

---
+++ test/infra/repositories/mongodb/mongodb-user-repository.spec.ts
@@ -0,0 +1,40 @@
+import { MongodbUserRepository } from '@/infra/repositories/mongodb'
+import { MongoHelper } from '@/infra/repositories/mongodb/helper'
+
+describe('Mongodb User Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(globalThis.__MONGO_URI__)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    MongoHelper.clearCollection('users')
+  })
+
+  test('when user is added, it should exist', async () => {
+    const userRepository = new MongodbUserRepository()
+    const user = {
+      name: 'Any Name',
+      email: 'any@email.com'
+    }
+    await userRepository.add(user)
+    expect(await userRepository.exists(user)).toBeTruthy()
+  })
+
+  test('findAllUsers should return all added users', async () => {
+    const userRepository = new MongodbUserRepository()
+    await userRepository.add({
+      name: 'Any Name',
+      email: 'any@email.com'
+    })
+    await userRepository.add({
+      name: 'Second Name',
+      email: 'second@email.com'
+    })
+    const users = await userRepository.findAllUsers()
+    expect(users[0].name).toEqual('Any Name')
+    expect(users[1].name).toEqual('Second Name')
+  })
+})
