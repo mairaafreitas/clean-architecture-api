9d4e6a800790543d0aa2661647d22de8114ece11
mairaafreitas <maira.oliveirafreitas@gmail.com>
2023-10-05 08:51:33 -0300

    feat: nodemailer adapter

---
+++ src/infra/mail-services/nodemailer-email-service.ts
@@ -0,0 +1,30 @@
+import { right, type Either, left } from '@/shared'
+import { MailServiceError } from '@/usecases/errors'
+import { type EmailOptions, type EmailService } from '@/usecases/send-email/ports'
+import * as nodemailer from 'nodemailer'
+
+export class NodemailerEmailService implements EmailService {
+  async send (options: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
+    try {
+      const transporter = nodemailer.createTransport({
+        host: options.host,
+        port: options.port,
+        auth: {
+          user: options.username,
+          password: options.password
+        }
+      })
+      await transporter.sendMail({
+        from: options.from,
+        to: options.to,
+        subject: options.subject,
+        text: options.text,
+        html: options.html,
+        attachments: options.attachments
+      })
+    } catch (error) {
+      left(new MailServiceError())
+    }
+    return right(options)
+  }
+}
