f76520e88bd75172fa00d0f583b9bd974d40bb48
mairaafreitas <maira.oliveirafreitas@gmail.com>
2023-10-06 08:19:56 -0300

    feat: add usecases to controller

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,9 +1,17 @@
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { RegisterAndSendEmailController } from '@/web-controllers'
 import { MongodbUserRepository } from '@/infra/repositories/mongodb'
+import { SendEmail } from '@/usecases/send-email'
+import { NodemailerEmailService } from '@/infra/mail-services'
+import { getEmailOptions } from '@/main/config/email'
+import { RegisterAndSendEmail } from '@/usecases/register-and-send-email'
 
 export const makeRegisterAndSendEmailController = (): RegisterAndSendEmailController => {
   const mongoDbUserRepository = new MongodbUserRepository()
-  const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(mongoDbUserRepository)
-  return new RegisterAndSendEmailController(registerUserOnMailingListUseCase)
+  const registerUserOnMailingList = new RegisterUserOnMailingList(mongoDbUserRepository)
+  const emailService = new NodemailerEmailService()
+  const sendEmailUsecase = new SendEmail(getEmailOptions(), emailService)
+  const registerAndSendEmailUseCase = new RegisterAndSendEmail(registerUserOnMailingList, sendEmailUsecase)
+  const registerUserOnMailingListController = new RegisterAndSendEmailController(registerAndSendEmailUseCase)
+  return registerUserOnMailingListController
 }
