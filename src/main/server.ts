1db7fbbfd425543f30c2643f0abb157540a8345a
mairaafreitas <maira.oliveirafreitas@gmail.com>
2023-10-11 08:24:35 -0300

    fix: mongo envs on server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,9 @@
 import 'module-alias/register'
 import { MongoHelper } from '@/infra/repositories/mongodb/helper'
 
-MongoHelper.connect(process.env.MONGO_URL).then(async () => {
+MongoHelper.connect(process.env.MONGO_URL ?? '').then(async () => {
   const app = (await import('./config/app')).default
-  app.listen(5000, () => {
+  app.listen(process.env.MONGO_PORT ?? 5000, () => {
     console.log('Server running at http://localhost/5000')
   })
 }).catch(console.error)
